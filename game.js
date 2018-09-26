d799f4eb2550de210ab0256cc350dba9f188fad1
abdulazizasiri <aasiri@lion.lmu.edu>
2018-09-26 15:24:31 -0700

    few changes

--- game.js
+++ game.js
@@ -1,5 +1,14 @@
 // Same as: $(function() {});
 
 $( document ).ready(function() {
+  var container = $('#container');
+  var bird = $('#bird');
+  var wall = $('#wall');
+  var p1 = $('#p1');
+  var p2 = $('#p2');
+  var score = $('#score');
 
+
+  var container_width = parseInt(container.height());
+  // alert(container_width)
 });
